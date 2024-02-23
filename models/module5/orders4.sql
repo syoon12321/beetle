1ce318d895c4747621c40b3804789f4cd24df727
Soohyun Yoon <s.yoon@revolentgroup.com>
2024-02-23 17:39:27 +0000

    Added incremental model

--- models/module5/orders4.sql
+++ models/module5/orders4.sql
@@ -4,8 +4,18 @@
     )
 }}
 
-select
-    *,
-    current_timestamp() as audit_insert_dttm,
-    current_timestamp() as audit_update_dttm
-from {{ source('staging', 'stg_raw__il_order') }}
+with final as (
+    select
+        *,
+        current_timestamp() as audit_insert_dttm,
+        current_timestamp() as audit_update_dttm
+    from {{ ref('stg_raw__il_order') }}
+
+    {% if is_incremental() %}
+
+    where o_updatedate > (select max(o_updatedate) from {{this}})
+
+    {% endif %}
+)
+
+select * from final
